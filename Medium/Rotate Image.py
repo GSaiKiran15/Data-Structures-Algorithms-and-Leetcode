69cbffeaba169217cf8bc089931e8c85487cb790
Sai Kiran Ganesh Kumar <ganeshsaikiran7@gmail.com>
2025-08-01 22:29:29 -0600

    '*'

--- Medium/Rotate Image.py
+++ Medium/Rotate Image.py
@@ -1,9 +1,12 @@
 matrix = [[1,2,3],[4,5,6],[7,8,9]]
-h = 0
-while h < len(matrix):
-    for i in range(len(matrix[:])):
-        for j in range(len(matrix[:])):
-            matrix[h][j] = matrix[j][i]
-        h += 1  
-
+for i in range(len(matrix)):
+    top = 0
+    bottom = len(matrix) - 1
+    while bottom > top:
+        matrix[bottom][i], matrix[top][i] = matrix[top][i], matrix[bottom][i]
+        top += 1
+        bottom -= 1
+    for row in range(len(matrix)):
+        for col in range(row + 1, len(matrix)):
+            matrix[row][col], matrix[col][row] = matrix[col][row], matrix[row][col]
 print(matrix)
