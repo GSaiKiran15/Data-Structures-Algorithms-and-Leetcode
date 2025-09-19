cca6815b64514536c151600bd49d0b35db99719d
Ganesh Sai Kiran <ganeshsaikiran7@gmail.com>
2025-09-18 21:45:15 -0600

    -

---
+++ Medium/Flatten Binary Tree to Linked List.py
@@ -0,0 +1,17 @@
+def flatten(root):
+    
+    def dfs(root):
+        if not root:
+            return None
+
+        leftTail = dfs(root.left)
+        rightTail = dfs(root.right)
+        
+        if root.left:
+            leftTail.right = root.right
+            root.right = root.left
+            root.left = None
+        last = rightTail or leftTail or root
+        return last
+    dfs(root)
+        
