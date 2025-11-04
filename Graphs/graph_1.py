30aa1a82943b4ec832d199758bd9100e47da0c2e
Ganesh Sai Kiran <ganeshsaikiran7@gmail.com>
2025-11-04 00:07:02 -0700

    -

--- Graphs/graph_1.py
+++ Graphs/graph_1.py
@@ -25,11 +25,19 @@ class Graph:
         return paths
 
     def get_shortest_path(self, start, end, path=[]):
-        path += [start]
-        if start not in self.graph_dict:
-            return None
+        path = path + [start]
         if start == end:
             return [path]
+        if start not in self.graph_dict:
+            return None
+        shortest_path = None
+        for node in self.graph_dict[start]:
+            if node not in path:
+                sp = self.get_shortest_path(node, end, path)
+                if sp:
+                    if not shortest_path or len(sp) < len(shortest_path):
+                        shortest_path = sp
+        return shortest_path
         
 
 if __name__ == '__main__':
@@ -44,3 +52,4 @@ if __name__ == '__main__':
 
 route_graph = Graph(routes)
 route_graph.get_paths("Mumbai", "Toronto")
+print(route_graph.get_shortest_path("Mumbai", "Toronto"))
---
+++ Medium/Number of Islands.py
@@ -0,0 +1,27 @@
+import collections 
+def numIslands(grid):
+    if not grid:
+        return 0
+    
+    rows, cols = len(grid), len(grid[0])
+    visited = set()
+    islands = 0
+
+    def bfs(r,c):
+        q = collections.deque()
+        visited.add((r,c))
+        q.append((r,c))
+        while q:
+            row, col = q.popleft()
+            directions = [[1,0], [-1 ,0], [0, 1], [0, -1]]
+            for dr, dc in directions:
+                r, c = row + dr, col + dc
+                if (r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r,c) not in visited):
+                    q.append((r,c))
+                    visited.add((r,c))
+    for r in range(rows):
+        for c in range(cols):
+            if ((r,c) not in visited and grid[r][c] == "1"):
+                bfs(r,c)
+                islands += 1
+    return islands
