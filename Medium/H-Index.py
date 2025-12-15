a26b4600c9ffb8c6048fb1bb748b2d6f0dcbf69e
Ganesh Sai Kiran <ganeshsaikiran7@gmail.com>
2025-12-14 21:01:34 -0700

    -

--- Medium/H-Index.py
+++ Medium/H-Index.py
@@ -1,36 +1,15 @@
 citations = [3,0,6,1,5]
+h = 0
+paper_counts = [0]*(len(citations) + 1)
 
-n = len(citations)
-
-paper_counts = [0] * (n+1)
-
-for c in citations:
-    paper_counts[min(n,c)] += 1
-
-h = n
-
-papers = paper_counts[n]
-
-while papers < h:
-    h -= 1
-    papers += paper_counts[h]
-
-print(h)
-
-
-
-# l = [0] * (len(citations)+1)
-# for i in citations:
-#     if i > len(l)-1:
-#         l[-1] += 1
-#     else:
-#         l[i] += 1
-# output = 0
-# for i in range(len(l)-1, -1, -1):
-#     output += l[i]
-#     if i >= output:
-#         print(l[i], output)
-# else:
-#     print(0)
-    
-    
+for i in range(len(citations)):
+    if citations[i] > len(citations):
+        paper_counts[-1] += 1
+    else:
+        paper_counts[citations[i]] += 1
+sum = 0
+for i in range(len(paper_counts)-1, -1, -1):
+    sum += paper_counts[i]
+    if sum >= i:
+        print(i)
+        break
