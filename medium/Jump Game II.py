373aa8f1c872f9612510e6c275c2a8ebd17d9b41
Ganesh Sai Kiran <ganeshsaikiran7@gmail.com>
2025-12-29 22:16:36 -0700

    -

---
+++ medium/Jump Game II.py
@@ -0,0 +1,11 @@
+nums = [2, 3, 1, 1, 4]
+l = r = 0
+res = 0
+while r < len(nums) - 1:
+    farthest = 0
+    for i in range(l, r+1):
+        farthest = max(farthest, i + nums[i])
+    l = r + 1
+    r = farthest
+    res += 1
+print(res)
