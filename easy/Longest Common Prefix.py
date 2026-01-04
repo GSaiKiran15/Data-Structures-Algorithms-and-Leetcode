1c1077a7ceed1b11dcfd94f62507084abcb39483
Ganesh Sai Kiran <ganeshsaikiran7@gmail.com>
2026-01-04 15:58:27 -0700

    -

---
+++ easy/Longest Common Prefix.py
@@ -0,0 +1,10 @@
+strs = ["flower","flow","flight"]
+
+res = ""
+
+for i in range(len(strs[0])):
+    for s in strs:
+        if i == len(s) or s[i] != strs[0][i]:
+            print(res)
+            break
+    res += strs[0][i]
