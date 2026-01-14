68376a01ad9c8bfd63160599f8460caa420e58c6
Ganesh Sai Kiran <ganeshsaikiran7@gmail.com>
2026-01-13 20:52:06 -0700

    *

---
+++ hard/Candy.py
@@ -0,0 +1,9 @@
+ratings = [1,6,10,8,7,3,2]
+candies = [1] * len(ratings)
+for i in range(1, len(ratings)):
+    if ratings[i] > ratings[i-1]:
+        candies[i] = candies[i - 1] + 1
+for i in range(len(ratings)-2, -1, -1):
+    if ratings[i] > ratings[i + 1]:
+        candies[i] = max(candies[i], candies[i + 1] + 1)
+print(sum(candies))
