f1f40abcd4e8c1ab78500ffaf4257524a28fc011
Ganesh Sai Kiran <ganeshsaikiran7@gmail.com>
2026-01-14 14:35:02 -0700

    need to learn again

---
+++ hard/Trapping Rain Water.py
@@ -0,0 +1,16 @@
+height = [0,1,0,2,1,0,1,3,2,1,2,1]
+l, r = 0, len(height) - 1
+leftMax, rightMax = height[l], height[r] 
+res = 0
+
+while l < r:
+    if leftMax < rightMax:
+        l += 1
+        leftMax = max(leftMax, height[l])
+        res += leftMax - height[l]
+    else:
+        r -= 1 
+        rightMax = max(rightMax, height[r])
+        res += rightMax - height[r]
+
+print(res)
---
+++ hard/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+f l <= len(height) - 1:
+#     total -= (len(height) - 1 - l) * (height[l] - height[r])
+#     res = total
